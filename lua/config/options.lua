b7606ff15e1a4e6eeb63d427544bd34810009068
Arka Banerjee <arkabanerjee381@gmail.com>
2025-04-28 01:12:35 +0530

    nvim config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+--
+
+-- lua/config/options.lua
+
+-- Other options above...
+
+-- Set colorscheme (optional if you're already doing it in colorscheme.lua)
+--vim.g.colors_name = "rose-pine"
+
+-- You can also set any additional vim options here
+--vim.o.termguicolors = true
